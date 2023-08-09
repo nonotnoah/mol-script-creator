019efa1a0f1ff69701cf3f98f16bad6e339d1844
Noah Myers <noahm@mcmenamins.com>
2023-08-09 13:02:22 -0700

    add default summary property to script init

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -39,7 +39,7 @@ export default function Home() {
               <Link to={`script/new`} state={{
                 id: nanoid(5),
                 script: [],
-                info: { title: 'Untitled Script', description: '', characters: [], locations: [], start: 700, end: 2300 }
+                info: { summary: '', title: 'Untitled Script', description: '', characters: [], locations: [], start: 700, end: 2300 }
               }}>
                 <div
                   className="project-wrapper"
